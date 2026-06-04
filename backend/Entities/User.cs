a3f1884e10ec1110c1edfe084262aa080a100039
Perelik88 <marinmstefanov@gmail.com>
2026-06-04 11:31:42 +0300

    Revert "first commit"
    This reverts commit 51a0b7b8c1c9df99c20cc0b83e8950d914a746e2.

--- backend/Entities/User.cs
+++
@@ -1,8 +0,0 @@
-namespace backend.Entities;
-
-public class User
-{
-    public string Id { get; set; } = Guid.NewGuid().ToString();
-    public required string DisplayName { get; set; }
-    public required string Email { get; set; }
-}
